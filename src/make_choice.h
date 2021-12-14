6ed3d97af1a7842f3fbd5f467e255f244d97e2fa
gstathos1 <90729947+gstathos1@users.noreply.github.com>
2021-12-13 23:46:16 -0500

    Update make_choice.h

--- src/make_choice.h
+++ src/make_choice.h
@@ -1,1 +1,1 @@
-void makeChoice(int *platform, int *progress, int *lives, char *row1, char *row2);
+void makeChoice(int *platform, int *progress, int *lives, char *row1, char *row2);        /* George S */
